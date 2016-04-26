2a2869790bc104e7685c1d44476194d21a3924e2
Falcon Taylor-Carter <falconertc@colorado.edu>
2016-04-26 11:43:25 -0600

    Change grid to scale

--- HuddleSimulation/Grid.py
+++ HuddleSimulation/Grid.py
@@ -1,3 +1,6 @@
+import math as Math
+import sys
+
 import matplotlib.pyplot as plt
 
 
@@ -11,8 +14,6 @@ class Grid(object):
         plt.ion()
         self.figure, self.ax = plt.subplots()
         self.lines, = self.ax.plot([], [], 'o')
-        self.ax.set_xlim([-5, 305])
-        self.ax.set_ylim([-5, 305])
         pass
 
     def update(self, penguins):
@@ -21,7 +22,29 @@ class Grid(object):
             x_vals.append(penguin.x)
             y_vals.append(penguin.y)
 
+        window = 100.0
         self.lines.set_xdata(x_vals)
+        max_x, min_x = max(x_vals), min(x_vals)
+        self.ax.set_xlim([Math.floor(min_x / window) * window,
+                         Math.ceil(max_x / window) * window])
+
         self.lines.set_ydata(y_vals)
+        max_y, min_y = max(x_vals), min(x_vals)
+        self.ax.set_ylim([Math.floor(min_y / window) * window,
+                         Math.ceil(max_y / window) * window])
         self.figure.canvas.draw()
         pass
+
+    def max(self, data):
+        max_val = 0
+        for value in data:
+            if value > max_val:
+                max_val = value
+        return max_val
+
+    def min(self, data):
+        max_val = sys.maxint
+        for value in data:
+            if value < max_val:
+                max_val = value
+        return max_val
